ffb340f0f7e7b899b39b80db39e143d616cc2526
alcino.fujii <yuki.fujii@hotmail.com>
2018-06-07 16:24:26 -0300

    Criação de telas cadastrar

--- app/src/main/java/com/br/kjtrufas/Administrador.java
+++ app/src/main/java/com/br/kjtrufas/Administrador.java
@@ -1,13 +1,25 @@
 package com.br.kjtrufas;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Administrador extends AppCompatActivity {
+
+    private Button btnCadastro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_administrador);
+
+        btnCadastro = (Button) findViewById(R.id.btnCadastrar);
+    }
+
+    public void chamarCadastro (View view) {
+        Intent it = new Intent(this, Cadastro.class);
+        startActivityForResult(it, 0);
     }
 }
